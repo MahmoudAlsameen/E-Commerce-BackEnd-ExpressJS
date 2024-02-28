3f03c514a2555d441b8e24fd428e135913c391b5
Mahmoud Alsameen <mahmoud_abdulrahman1@outlook.com>
2024-02-28 23:10:03 +0200

    add productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,35 @@
 import express from "express";
-import Joi from "joi";
+import {
+  addProduct,
+  getAllProducts,
+  getOneProduct,
+  getProductsPagination,
+  updateProduct,
+} from "../controllers/product.controller.js";
 import auth from "../middleware/auth.js";
 import validation from "../middleware/validation.js";
+import {
+  addProductValidationSchema,
+  updateProductValidationSchema,
+} from "../validation/product.validation.js";
 const productRoutes = express.Router();
 
-productRoutes.post("/", validation(Joi.object(), "body"), () => {});
+productRoutes.post(
+  "/",
+  [auth, validation(addProductValidationSchema, "body")],
+  addProduct
+);
 
-productRoutes.get("/", auth, () => {});
+productRoutes.get("/", getAllProducts);
+
+productRoutes.get("/:page", getProductsPagination);
+
+productRoutes.get("/:id", getOneProduct);
+
+productRoutes.patch(
+  "/:id",
+  validation(updateProductValidationSchema, "body"),
+  updateProduct
+);
 
 export default productRoutes;
